ee739fa717c6f2b952ee85c0514bd64210161ae3
hisunho <peller02@naver.com>
2023-02-01 16:12:50 +0900

    데스크탑에서 첫 커밋, 푸쉬

--- shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -12,4 +12,8 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
 }
